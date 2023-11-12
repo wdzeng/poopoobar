c6b8a92662905bce6dba390a1ba1090011c48d68
hyperbola <me@hyperbola.me>
2023-11-12 16:45:37 +0800

    fix: fail to import Options interface

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,5 @@
-export { ProgressBar, Options, MIN_BAR_WIDTH } from './progress-bar'
+import { MIN_BAR_WIDTH, ProgressBar } from './progress-bar'
+
+import type { Options } from './progress-bar'
+
+export { ProgressBar, Options, MIN_BAR_WIDTH }
